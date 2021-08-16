1af56b4f038038bb2b1495f4d5085007ea0eaa1c
aozaki-kuro <aozaki.kuro@gmail.com>
2021-08-16 09:29:14 +0900

    update font awesome

--- components/previews/MarkdownPreview.tsx
+++ components/previews/MarkdownPreview.tsx
@@ -1,5 +1,5 @@
 import { useEffect, FunctionComponent } from 'react'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from 'axios'
 import useSWR from 'swr'
 import Prism from 'prismjs'
