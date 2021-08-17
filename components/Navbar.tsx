b079d55a35136c3874e7af47e857aabfc25ce313
aozaki-kuro <aozaki.kuro@gmail.com>
2021-08-17 15:59:14 +0900

    update git icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,8 @@ const Navbar = () => {
     <div className="text-left bg-white p-3 sticky top-0 bg-opacity-80 backdrop-blur-md shadow-sm z-[100]">
       <div className="max-w-4xl w-full mx-auto flex items-center justify-between">
         <h1 className="font-bold text-xl">{siteConfig.title}</h1>
-        <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={['fab', 'github']} size="lg" />
+        <a href="https://www.youtube.com/channel/UC5CwaMl1eIgY8h02uZw7u8A" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={['fas', 'fa-meteor']} size="lg" />
         </a>
       </div>
     </div>
