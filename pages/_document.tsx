13979bbff8be8ee4e6edfc6a79eed72343983b8e
aozaki-kuro <aozaki.kuro@gmail.com>
2021-08-17 15:19:09 +0900

    update title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,12 +6,12 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta property="og:url" content="https://archive.suisei.cc/" />
-          <meta property="og:title" content="Suisei Archive" />
+          <meta property="og:title" content="Suisei Archive | Treasure every moment of our dear comet" />
           <meta property="og:image" content="https://img.suisei.cc/icon.png" />
           <meta name="description" content="Suisei YouTube Archive. Hosted on Vercel." />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:description" content="The archiving site of Hoshimachi Suisei's YouTube stream and videos. Hosted on Vercel. This is a personal project, DO NOT DISTRIBUTE." />
-          <meta name="twitter:title" content="Suisei Archive | Treasure every moment of our dear comet." />
+          <meta name="twitter:title" content="Suisei Archive | Treasure every moment of our dear comet" />
           <meta name="twitter:image" content="https://img.suisei.cc/icon.png" />
           <link rel="apple-touch-icon" href="/android-chrome-192x192.png" sizes="192x192" />
           <link rel="manifest" href="/static/manifest.json" />
