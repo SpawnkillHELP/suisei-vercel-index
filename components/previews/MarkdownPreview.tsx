bb465bb8eb348eff76b11aaba744402a447ab2be
Aozaki <aozaki.kuro@gmail.com>
2021-08-24 10:27:37 +0800

    Update MarkdownPreview.tsx

--- components/previews/MarkdownPreview.tsx
+++ components/previews/MarkdownPreview.tsx
@@ -17,9 +17,7 @@ import DownloadBtn from '../DownloadBtn'
 const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
 const MarkdownPreview: FunctionComponent<{ file: any; standalone?: boolean }> = ({ file, standalone = true }) => {
-  const { data, error } = useSWR(file['@microsoft.graph.downloadUrl'], fetcher, {
-    revalidateOnFocus: false,
-  })
+  const { data, error } = useSWR(file['@microsoft.graph.downloadUrl'], fetcher)
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
