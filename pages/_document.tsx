45ffcc96ded8eb2f2d7aa8f139f4ea3d6e823eee
Aozaki <aozaki.kuro@gmail.com>
2021-09-19 10:14:37 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta property="og:url" content="https://archive.suisei.cc/" />
-          <meta property="og:title" content="Suisei Archive | Treasure every moment of her" />
+          <meta property="og:title" content="Suisei Archive | Treasure every moment" />
           <meta property="og:image" content="https://img.suisei.cc/icon.png" />
           <meta name="description" content="he archiving site of Hoshimachi Suisei's YouTube stream and videos. This is a personal project, DO NOT DISTRIBUTE." />
           <meta name="robots" content="noindex" />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:description" content="The archiving site of Hoshimachi Suisei's YouTube stream and videos. This is a personal project, DO NOT DISTRIBUTE." />
-          <meta name="twitter:title" content="Suisei Archive | Treasure every moment of her" />
+          <meta name="twitter:title" content="Suisei Archive | Treasure every moment" />
           <meta name="twitter:image" content="https://img.suisei.cc/icon.png" />
           <meta name="theme-color" content="#d072a5" />
           <link rel="apple-touch-icon" href="/android-chrome-192x192.png" sizes="192x192" />
